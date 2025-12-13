fcb3c36728b45698ac736176e4e0ce801ae50c65
Boris Mora <bmoraibarra@gmail.com>
2025-12-13 15:38:45 -0300

    Update

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,5 +1,5 @@
 export async function fetchCategories() {
-  const res = await fetch('/api/categories', {
+  const res = await fetch('/categories', {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' }
   });
