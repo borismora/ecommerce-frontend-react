7f45a683da655b0bf008c254a766a7cd98cef748
Boris Mora <bmoraibarra@gmail.com>
2025-12-13 19:31:45 -0300

    Update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 async function request(path, options = {}) {
-  const response = await fetch(`${API_BASE_URL}${path}`, {
+  const response = await fetch(`${API_BASE_URL}/api${path}`, {
     credentials: "include",
     headers: {
       "Content-Type": "application/json",
